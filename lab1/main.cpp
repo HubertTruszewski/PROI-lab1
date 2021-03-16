1de1bd7809513f50129eff458d05cba1e4c4c28f
Hubert Truszewski <01150052@pw.edu.pl>
2021-03-16 22:04:47 +0100

    Add multiply function definition and body

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "calcApp.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a, b;
+    std::cout << "Program sluzy do mnozenia dwoch liczb calkowitych\n";
+    std::cout << "Podaj pierwsza liczbe: ";
+    std::cin >> a;
+    std::cout << "Podaj druga liczbe: ";
+    std::cin >> b;
+    int result = multiply(a, b);
+    std::cout << "Wynik mnozenia podanych liczb to: " << result;
 }
